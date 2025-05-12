6289fffa30026668ef32389663f4687766f87660
Emanuel Pulgarin <erXiplay@gmail.com>
2025-05-12 06:49:25 -0500

    Feat(Models): add User model to index.js

--- src/models/index.js
+++ src/models/index.js
@@ -5,3 +5,4 @@ export { default as Asesores } from "./asesores.model.js";
 export { default as Proyectos } from "./proyectos.model.js";
 export { default as Estudiantes } from "./estudiantes.model.js";
 export { default as FechaAsesoria } from "./fechaAsesoria.model.js";
+export { default as Usuario } from "./usuarios.model.js";
