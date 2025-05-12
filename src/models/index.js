4e253bf74bbc543fd965cf7f7cd44f84238d8c6c
Emanuel Pulgarin <erXiplay@gmail.com>
2025-05-12 06:48:36 -0500

    Feat(Models): add AsesorDate model to index.js

--- src/models/index.js
+++ src/models/index.js
@@ -4,3 +4,4 @@ export { default as Carreras } from "./carreras.model.js";
 export { default as Asesores } from "./asesores.model.js";
 export { default as Proyectos } from "./proyectos.model.js";
 export { default as Estudiantes } from "./estudiantes.model.js";
+export { default as FechaAsesoria } from "./fechaAsesoria.model.js";
