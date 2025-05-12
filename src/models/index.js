57e70d69cf549ef728fea21a11175e09398b848c
Emanuel Pulgarin <erXiplay@gmail.com>
2025-05-12 06:42:23 -0500

    Feat(Models): add Projects  model to index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,5 @@
 
 //File to import all models
 export { default as Carreras } from "./carreras.model.js";
+export { default as Asesores } from "./asesores.model.js";
+export { default as Proyectos } from "./proyectos.model.js";
