bc0585ecca0d6d7a2ca60aa11508c0c36c402546
Emanuel Pulgarin <erXiplay@gmail.com>
2025-05-12 08:26:38 -0500

    Feat(Routes): Add All routes in index.js

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+// routes/index.js
+export { default as carreraRouter } from "./carreras.routes.js";
+export { default as asesoresRoutes } from "./asesores.routes.js";
+export { default as proyectosRoutes } from "./proyectos.routes.js";
+export { default as estudiantesRouter } from "./estudiantes.routes.js";
+export { default as fechaAsesoriaRouter } from "./fechaAsesoria.routes.js";
+export { default as informesRouter } from "./informes.routes.js";
+export { default as usuariosRouter } from "./usuarios.routes.js";
